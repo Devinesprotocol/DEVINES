db8560bface2e7f25a7f32c5d5377b33fa494329
Devines Protocol <devineverse@proton.me>
2026-03-06 19:11:17 -0300

    Update script.js
    Signed-off-by: Devines Protocol  <devineverse@proton.me>

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,9 +1,57 @@
-function openPantheon(name){
+let pantheon = null
+let entity = null
 
-if(name === "greek"){
 
-window.location.href = "greek.html"
+function selectPantheon(name){
+
+pantheon = name
+
+localStorage.setItem("pantheon",name)
+
+window.location = "/pantheon.html"
 
 }
+
+
+function selectEntity(name){
+
+entity = name
+
+localStorage.setItem("entity",name)
+
+window.location = "/chat.html"
+
+}
+
+
+async function sendMessage(){
+
+const message = document.getElementById("message").value
+
+const response = await fetch("/chat",{
+
+method:"POST",
+
+headers:{"Content-Type":"application/json"},
+
+body:JSON.stringify({
+
+pantheon:localStorage.getItem("pantheon"),
+
+entity:localStorage.getItem("entity"),
+
+message:message
+
+})
+
+})
+
+const data = await response.json()
+
+const chat = document.getElementById("chat-window")
+
+chat.innerHTML += "<p><b>You:</b> "+message+"</p>"
+
+chat.innerHTML += "<p><b>"+data.entity+":</b> "+data.response+"</p>"
 
 }
